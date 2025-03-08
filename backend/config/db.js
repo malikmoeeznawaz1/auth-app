08dc2939315726a2ed5e8d94c3549a0fa6a2b03a
Malik Moeez Nawaz <malikmoeez.nawaz@studentambassadors.com>
2025-03-09 03:00:24 +0500

    db.js and env file configured

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,12 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+  try{
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected Successfully!");
-  } catch (error) {
-    console.error("MongoDB Connection Error:", error);
+  }
+  catch(error){
+    log.error("MongoDB Connection Error:", error);
     process.exit(1);
   }
 };
